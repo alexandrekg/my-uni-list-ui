4aa5d8dbe0f50efc307a1c3e0ac66d0e1c21b918
Alexandre Kuns Giacobo <alexandregiacobok@outlook.com>
2022-07-19 13:33:06 -0300

    add content wrapper

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
       <header className='header'>
         <h1>Piarzinho Mangá List</h1>
       </header>
+      <div className='content-wrapper'>
+        <div className='content-list'>
+          <div className='content-list-item'>
+            <span>Example #1</span>
+            <span>Example #2</span>
+            <span>Example #3</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
