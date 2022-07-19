cf8f2d1eeaab139cab89d7fe8ea41443567fcdd9
Alexandre Kuns Giacobo <alexandregiacobok@outlook.com>
2022-07-19 13:37:52 -0300

    add three examples

--- src/App.js
+++ src/App.js
@@ -9,9 +9,19 @@ function App() {
       <div className='content-wrapper'>
         <div className='content-list'>
           <div className='content-list-item'>
-            <span>Example #1</span>
-            <span>Example #2</span>
-            <span>Example #3</span>
+            <span>Naruto</span>
+            <span>700/700</span>
+            <span>finished</span>
+          </div>
+          <div className='content-list-item'>
+            <span>In Another World With my Smartphone</span>
+            <span>--/--</span>
+            <span>to read</span>
+          </div>
+          <div className='content-list-item'>
+            <span>Accel World</span>
+            <span>20/--</span>
+            <span>reading</span>
           </div>
         </div>
       </div>
